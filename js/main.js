2c4e8b1540a8240caa71c8b0ebb2f29b4c5b1440
alsomslo <133613694+alsomslo@users.noreply.github.com>
2023-07-03 18:17:39 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,20 @@ $(function () {
     sct > 0 ? $('.header').addClass('on') : $('.header').removeClass('on');
   })
 
+
+  $('.rgh li').on('click', function () {
+    $('.rgh li').toggleClass('on').siblings().removeClass('on')
+  })
+
+  $('.mainslide').slick({
+
+    autoplay: 5000,
+    speed: 500,
+    dots: true,
+    arrows: false,
+
+  })
+
   $('.maintap li').on('click', function (e) {
     e.preventDefault();
     let idx = $(this).index();
@@ -13,4 +27,43 @@ $(function () {
 
 
 
-})
+
+
+  $('.cases').slick({
+    centerMode: true,
+    centerPadding: '100px',
+    arrows: false,
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '30px',
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '30px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.mainmenu li').on('click', function (e) {
+    e.preventDefault();
+    let idx = $(this).index();
+    $('.mainmenu li').eq(idx).addClass('on').siblings().removeClass('on');
+    $('.pz_menu li').eq(idx).addClass('on').siblings().removeClass('on');
+  })
+
+
+
+
+});
