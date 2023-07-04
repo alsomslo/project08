2076b498d25077d3445e1894222e5a6abfc7ed5a
alsomslo <133613694+alsomslo@users.noreply.github.com>
2023-07-04 18:30:49 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,11 +18,11 @@ $(function () {
 
   })
 
-  $('.maintap li').on('click', function (e) {
+  $('.mainproduct li').on('click', function (e) {
     e.preventDefault();
     let idx = $(this).index();
-    $('.maintap li').eq(idx).addClass('on').siblings().removeClass('on');
-    $('.tap_detail li').eq(idx).addClass('on').siblings().removeClass('on');
+    $('.mainproduct li').eq(idx).addClass('on').siblings().removeClass('on');
+    $('.left_box li').eq(idx).addClass('on').siblings().removeClass('on');
   })
 
 
